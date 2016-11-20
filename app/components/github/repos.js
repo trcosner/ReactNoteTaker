5eb8a68d5f4f389e07d852423bd90acfef5c490d
trcosner <trcosner@gmail.com>
2016-11-20 16:59:14 -0500

    Route transitions and propType validation

--- app/components/github/repos.js
+++ app/components/github/repos.js
@@ -1,11 +1,15 @@
 var React = require('react');
 
 var Repos = React.createClass({
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    repos: React.PropTypes.array.isRequired
+  },
   render: function(){
     return(
       <div>
         <p>Repos</p>
-        Repos {this.props.repos}  
+        Repos {this.props.repos}
       </div>
     )
   }
