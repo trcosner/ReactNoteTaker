f9b78614b692796ac38be04b5cec9cb00ef88a46
trcosner <trcosner@gmail.com>
2016-11-20 22:50:37 -0500

    stateless function components

--- app/components/home.js
+++ app/components/home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-class Home extends React.Component {
-  render(){
-    return(
-      <h2 className="text-center">
-        Search By Github Username Above
-      </h2>
-    )
-  }
+//if component doesnt have any state and only has render method
+//we can use statless function components
+
+const Home = () => {
+  return(
+    <h2 className="text-center">
+      Search By Github Username Above
+    </h2>
+  )
 }
 
 export default Home
